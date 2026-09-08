2dc2d5c766454d90ce05e40ca08e6466b871833d
Ota Seiji <tky2502060@stu.o-hara.ac.jp>
2026-09-08 14:10:53 +0900

    コメント文字化け直し

--- Source/Player.h
+++ Source/Player.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 #include "Object2D.h"
+#include "GameConfig.h"
 
 class Collider;
 
@@ -21,11 +22,11 @@ private:
 
 	float mfSpeed = 10.0f;
 
-	float mCurrentAngle = 0.0f; // ���̊p�x�i�x���@�F0?360�j
-	float mTargetAngle = 0.0f;  // �ڎw���p�x
+	float mCurrentAngle = 0.0f; // 現在の角度
+	float mTargetAngle = 0.0f;  // 目標の角度
 
-	const float BLOCK_SIZE = 100.0f; // �u���b�N�̃T�C�Y�i����j
-	float mMoveStepX = 0.0f; // �ړ��X�e�b�v�i����j
+	const float BLOCK_SIZE = GameConfig::CELL_SIZE;
+	float mMoveStepX = 0.0f;	// 回転に伴う移動値
 
 	void Rotate();
 };
